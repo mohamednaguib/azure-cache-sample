a86335ecfc49f6f6dc2cc4bda20f4a6af98bb985
Mohamed Naguib <mohamed.naguib@socialfruits.com>
2014-06-27 18:01:01 +0200

    push project

---
+++ AzureCacheSample/AzureCacheSample/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationServer.Caching;
+
+namespace AzureCacheSample
+{
+    public class Program
+    {
+        public static DataCache cache = new DataCache("default");
+        static void Main(string[] args)
+        {
+
+            SaveToCache("AzureCacheSample", "AzureCacheSampleValue");
+            string temp = GetFromCache("AzureCacheSample").ToString();
+        }
+        public static void SaveToCache(string key,string value)
+        {
+            cache.Put(key, value);
+        }
+        public static object GetFromCache(string key)
+        {
+            return cache.Get(key);
+        }
+    }
+}
+
